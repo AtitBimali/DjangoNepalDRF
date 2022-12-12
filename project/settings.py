70bb9518f635b3404fcf99147938950bafb7e154
AtitBimali <atitbimali10@gmail.com>
2022-12-12 17:04:45 +0545

    static

--- project/settings.py
+++ project/settings.py
@@ -123,7 +123,7 @@ import os
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
