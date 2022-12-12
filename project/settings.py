98d9f213485ebce495cb6024e3aeeda711160931
AtitBimali <atitbimali10@gmail.com>
2022-12-12 15:45:11 +0545

    commit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gr)s1m7&%gdh8clzshs_1kqymb+lx3bo=kju%^nuo3uuz_!a*h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangonepaldrf.pythonanywhere.com']
+ALLOWED_HOSTS = ['djangonepaldrf.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -123,7 +123,6 @@ import os
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
