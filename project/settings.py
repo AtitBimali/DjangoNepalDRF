02d91f433ac52d7e45a298d1c418c7c2dc41c1dc
AtitBimali <atitbimali10@gmail.com>
2022-12-12 15:32:12 +0545

    commit

--- project/settings.py
+++ project/settings.py
@@ -117,11 +117,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
